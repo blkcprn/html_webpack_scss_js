6d8f3acbe64affede4db6b20794840319b21ddef
blkcprn <blkcprn@gmail.com>
2024-11-13 08:02:26 +0200

    update js

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,12 @@ import "swiper/css/pagination";
 
 class Fixed {
     constructor(options={}) {
-        this.triggerSelector = options.triggerSelector ?? "[data-open-btn]"
+        this.triggerSelector = options.triggerSelector ?? "[data-open-btn]";
+        this.closeSelector = options.closeSelector ?? "[data-close-btn]";
+        this.overlaySelector = options.overlaySelector ?? ".overlay";
+        this.activeClass = options.activeClass ?? "active";
         this.fixedTriggers = document.querySelectorAll(this.triggerSelector);
-        this.overlay = document.querySelector(".overlay");
+        this.overlay = document.querySelector(this.overlaySelector);
         this.setup();
     }
     setup() {
@@ -27,35 +30,43 @@ class Fixed {
     }
     toggleFixed(elem, show) {
         if (show) {
-            elem.classList.add("active");
-            this.overlay.classList.add("active");
+            elem.classList.add(this.activeClass);
+            this.overlay.classList.add(this.activeClass);
             this.getLinks(elem).forEach((link) => {
-                link.setAttribute("tabindex", "0");
+                this.setTabindex(link, true);
             }); 
         } else {
-            elem.classList.remove("active");
-            this.overlay.classList.remove("active");
+            elem.classList.remove(this.activeClass);
+            this.overlay.classList.remove(this.activeClass);
             this.getLinks(elem).forEach((link) => {
-                link.setAttribute("tabindex", "-1");
-            }); 
+                this.setTabindex(link, false);
+            });
         }
     }
     getLinks(elem) {
         let links = [];
-        if (!elem.classList.contains("mobile-nav")) {
-            links = elem.querySelectorAll("a[tabindex]");
-        } else {
-            const menuLinks = document.querySelectorAll(".mobile-menu > li > a, .mobile-menu > li > h3 > a");
-            const enterlinks = document.querySelectorAll(".enter a");
-            links = [...menuLinks, ...enterlinks];
+        switch (elem.id) {
+            case "mobileMenu":
+                const menuLinks = elem.querySelectorAll(".mobile-menu__list > li > a, .mobile-menu__list > li > h3 > a");
+                const enterlinks = elem.querySelectorAll(".enter a");
+                links = [...menuLinks, ...enterlinks];
+                break;
+            default:
+                links = elem.querySelectorAll("a[tabindex]");
+                break;
         }
         return links;
     }
+    setTabindex(link, show, exceptClass=this.activeClass) {
+        if (!link.classList.contains(exceptClass)) {
+            link.setAttribute("tabindex", (show == true)? "0" : "-1");
+        }
+    }
     toggle(elem) {
-        const fixedId = elem.dataset.fixedTarget;
+        const fixedId = elem.dataset.target;
         const fixedElem = document.querySelector(`#${fixedId}`);
         this.toggleFixed(fixedElem, true);
-        const closeBtn = fixedElem.querySelector("[data-close-btn]");
+        const closeBtn = fixedElem.querySelector(this.closeSelector);
         closeBtn.addEventListener("click", () => {
             this.toggleFixed(fixedElem, false);
         });
@@ -69,8 +80,9 @@ class Fixed {
 class Dropdown {
     constructor(options={}) {
         this.dropdownSelector = options.dropdownSelector ?? ".dropdown";
-        this.toggleSelector = options.toggleSelector ?? ".dropdown-toggle";
-        this.contentSelector = options.contentSelector ?? ".dropdown-content";
+        this.toggleSelector = options.toggleSelector ?? ".dropdown__toggle";
+        this.contentSelector = options.contentSelector ?? ".dropdown__content";
+        this.accordionSelector = options.accordionSelector ?? "accordion-content";
         this.activeClass = options.activeClass ?? "active";
         this.dropdownElems = document.querySelectorAll(this.dropdownSelector);
         this.setup();
@@ -108,10 +120,23 @@ class Dropdown {
         const content = node.querySelector(this.contentSelector);
         this.setDropdownAttributes(toggle, content, show);
         this.toggleContentVisible(content, show);
+        this.setToggleActive(toggle, show);
     }
     clickEvent(toggle, content, show) {
         this.setDropdownAttributes(toggle, content, show);
         this.toggleContentVisible(content, show);
+        this.setToggleActive(toggle, show);
+    }
+    setToggleActive(toggle, show) {
+        if (show) {
+            if (!toggle.classList.contains(this.activeClass)) {
+                toggle.classList.add(this.activeClass);
+            }
+        } else {
+            if (toggle.classList.contains(this.activeClass)) {
+                toggle.classList.remove(this.activeClass);
+            }
+        }
     }
     setDropdownAttributes(toggle, content, show) {
         const expanded = (show == true)? "true" : "false";
@@ -119,28 +144,49 @@ class Dropdown {
         toggle.setAttribute("aria-expanded", expanded);
         content.setAttribute("aria-hidden", hidden);
 
-        if (content.classList.contains("minmenu-content")) {
+        if (content.classList.contains(this.accordionSelector)) {
             const liElems = content.querySelectorAll(":scope > li");
             liElems.forEach((el) => {
-                const aElem = el.querySelector("a[tabindex]");
-                aElem.setAttribute("tabindex", (show == true)? "0" : "-1");
+                const aElem = el.querySelector("a[tabindex], h3 > a[tabindex]");
+                this.setTabindex(aElem, show);
             });
         } else {
             const links = content.querySelectorAll("a[tabindex]");
             links.forEach((el) => {
-                el.setAttribute("tabindex", (show == true)? "0" : "-1");
+                this.setTabindex(el, show);
             });
         }        
     }
     toggleContentVisible(content, show) {
         if (show) {
             if (!content.classList.contains(this.activeClass)) {
+                if (content.classList.contains("tree-content") && content.dataset.level > 0) {
+                    const fitClass = this.getElemFitClass(content);
+                    content.classList.add(fitClass);
+                }
                 content.classList.add(this.activeClass);
             }
         } else {
             if (content.classList.contains(this.activeClass)) {
                 content.classList.remove(this.activeClass);
             }
+        }
+    }
+    getElemFitClass(elem) {
+        let fitClass = "to-right";
+        const contentRect = elem.getBoundingClientRect();
+        const windowWidth  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        if (contentRect.x - contentRect.width < 0) {
+            fitClass = "to-right";
+        }
+        if (contentRect.x + contentRect.width > windowWidth) {
+            fitClass = "to-left";
+        }
+        return fitClass;
+    }
+    setTabindex(link, show, exceptClass=this.activeClass) {
+        if (!link.classList.contains(exceptClass)) {
+            link.setAttribute("tabindex", (show == true)? "0" : "-1");
         }
     }
     toggle(elem) {
@@ -152,6 +198,94 @@ class Dropdown {
         this.toggleContentVisible(content, show);
     }
 }
+
+
+class Mode {
+    constructor(options={}) {
+        this.modeElem = document.querySelector("html");
+        this.btnSelector = options.btnSelector ?? "#mode-btn";
+        this.modeBtn = document.querySelector(this.btnSelector);
+        this.modes = ["light", "dark"];
+        this.mode = options.default ?? "light";
+        this.setup();
+    }
+    setup() {
+        let modeKey = localStorage.getItem("mode");
+        if (modeKey != null && modeKey != this.mode) {
+            this.mode = modeKey;
+            this.set(this.mode);
+        }
+        this.changeIcon(this.mode);
+        this.setClick();
+    }
+    setClick() {
+        this.modeBtn.addEventListener("change", (e) => {
+            const modeKey = this.getMode(e.target.checked);
+            this.setMode(modeKey);
+        });
+        this.modeBtn.addEventListener("keydown", (e) => {
+            if (e.keyCode == 13 || e.which === 13) {
+                e.target.checked = !e.target.checked;
+                const modeKey = this.getMode(e.target.checked);
+                this.setMode(modeKey);
+            }
+        });
+    }
+    getMode(isChecked) {
+        return isChecked ? "dark" : "light";
+    }
+    setMode(modeKey) {
+        if (modeKey) {
+            this.to(modeKey);
+        } else {
+            this.to();
+        }
+    }
+    setElemAttributes(elem) {
+        elem.setAttribute("aria-pressed", elem.checked === true ? "true" : "false");
+    }
+    getValidmodeKey(key) {
+        let modeKey = key
+        const modesKeys = Object.keys(this.modes)
+        if (!modesKeys.includes(key)) {
+            modeKey = this.mode
+        }
+        return modeKey
+    }
+    set(key) {
+        let modeKey = this.getValidmodeKey(key)
+        this.modeElem.setAttribute("data-mode", modeKey);
+    }
+    get() {
+        return this.mode;
+    }
+    next() {
+        /* If mode key is null - get next obj from modes obj */
+        const modesKeys = Object.keys(this.modes);
+        let modeInd = modesKeys.indexOf(this.mode);
+        const nextInd = (modeInd + 1) % modesKeys.length;
+        return modesKeys[nextInd];
+    }
+    to(key=null) {
+        /* if key if null. set next mode from modes */
+        if (this.mode == key) {return;}
+        const modeKey = (key == null)? this.next() : key;
+        this.set(modeKey);
+        this.modeElem.setAttribute("data-mode", modeKey);
+        localStorage.setItem("mode", modeKey);
+        this.mode = modeKey;
+        this.changeIcon(modeKey);
+    }
+    changeIcon(key) {
+        if (this.iconHandler) {
+            this.iconHandler(this.mode);
+        } else {
+            this.modeBtn.checked = (key == "dark") ? true : false;
+            this.setElemAttributes(this.modeBtn);
+        }
+    }
+}
+
 
 window.addEventListener("DOMContentLoaded", () => {
     const mode = new Mode();
@@ -173,3 +307,4 @@ window.addEventListener("DOMContentLoaded", () => {
         },
     });
 });
+
